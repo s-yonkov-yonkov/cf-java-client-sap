2fe27af597598cdb0f52db96ec7eecf0bef87aaf
i556696 <stefan.yonkov@sap.com>
2024-10-11 18:10:26 +0300

    test commit

--- src/main/java/com/sap/cloudfoundry/client/facade/ApplicationLogListener.java
+++ src/main/java/com/sap/cloudfoundry/client/facade/ApplicationLogListener.java
@@ -3,7 +3,7 @@ package com.sap.cloudfoundry.client.facade;
 import com.sap.cloudfoundry.client.facade.domain.ApplicationLog;
 
 public interface ApplicationLogListener {
-
+    //add a comment
     void onComplete();
 
     void onError(Throwable exception);
